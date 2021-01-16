0a2b5ae3f5de1de72ddd0471d05e30cdc0ecc6c3
Bucur Bogdan <bogdan.bucur103@yahoo.com>
2021-01-16 02:47:42 +0200

    Adaugarea clasei principale MainApp

---
+++ src/main/java/ro/mta/se/lab/MainApp.java
@@ -0,0 +1,41 @@
+package ro.mta.se.lab;
+
+import ro.mta.se.lab.controller.MainViewController;
+import ro.mta.se.lab.controller.ReadConfigController;
+import ro.mta.se.lab.model.CountryModel;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args )
+            {
+        launch(args);
+    }
+
+
+    public void start(Stage primaryStage) {
+
+        String path = "src/main/resources/config.cfg";
+        List<CountryModel> countryList = ReadConfigController.readConfigFile(path);
+        FXMLLoader loader = new FXMLLoader();
+
+        try {
+            loader.setLocation(this.getClass().getResource("/view/MainView.fxml"));
+            loader.setController(new MainViewController(countryList));
+            primaryStage.setScene(new Scene(loader.load()));
+            primaryStage.setTitle("Weather Forecast App");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
